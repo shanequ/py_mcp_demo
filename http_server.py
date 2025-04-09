56d77ad5416a1de3a605e7e39462c3689baae131
Shane Qu <shanequ@hotmail.com>
2025-04-10 09:50:38 +1000

    clean up code

--- http_server.py
+++ http_server.py
@@ -1,14 +1,10 @@
 # basic import 
-from mcp.server.fastmcp import FastMCP, Context
+from mcp.server.fastmcp import FastMCP
 import math
 
 from starlette.applications import Starlette
-from mcp.server.sse import SseServerTransport
-from starlette.requests import Request
-from starlette.routing import Mount, Route
-from mcp.server import Server
+from starlette.routing import Mount
 import uvicorn
-
 
 
 # instantiate an MCP server client
@@ -22,11 +18,13 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return int(a + b)
 
+
 # subtraction tool
 @mcp.tool()
 def subtract(a: int, b: int) -> int:
     """Subtract two numbers"""
     return int(a - b)
+
 
 # multiplication tool
 @mcp.tool()
@@ -34,11 +32,13 @@ def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return int(a * b)
 
+
 #  division tool
 @mcp.tool() 
 def divide(a: int, b: int) -> float:
     """Divide two numbers"""
     return float(a / b)
+
 
 # power tool
 @mcp.tool()
@@ -46,11 +46,13 @@ def power(a: int, b: int) -> int:
     """Power of two numbers"""
     return int(a ** b)
 
+
 # square root tool
 @mcp.tool()
 def sqrt(a: int) -> float:
     """Square root of a number"""
     return float(a ** 0.5)
+
 
 # cube root tool
 @mcp.tool()
@@ -58,11 +60,13 @@ def cbrt(a: int) -> float:
     """Cube root of a number"""
     return float(a ** (1/3))
 
+
 # factorial tool
 @mcp.tool()
 def factorial(a: int) -> int:
     """factorial of a number"""
     return int(math.factorial(a))
+
 
 # log tool
 @mcp.tool()
@@ -70,11 +74,13 @@ def log(a: int) -> float:
     """log of a number"""
     return float(math.log(a))
 
+
 # remainder tool
 @mcp.tool()
 def remainder(a: int, b: int) -> int:
     """remainder of two numbers divison"""
     return int(a % b)
+
 
 # sin tool
 @mcp.tool()
@@ -82,11 +88,12 @@ def sin(a: int) -> float:
     """sin of a number"""
     return float(math.sin(a))
 
-# cos tool
+
 @mcp.tool()
 def cos(a: int) -> float:
     """cos of a number"""
     return float(math.cos(a))
+
 
 # tan tool
 @mcp.tool()
@@ -94,8 +101,8 @@ def tan(a: int) -> float:
     """tan of a number"""
     return float(math.tan(a))
 
-# DEFINE RESOURCES
 
+# DEFINE RESOURCES
 @mcp.resource('list://resources')
 def list_resources() -> dict:
     """Return a list of all available resources in this server."""
@@ -148,45 +155,15 @@ def review_code(code: str) -> str:
     return f'Please review this code:\n\n{code}'
 
 
-def create_starlette_app(mcp_server: Server, *, debug: bool = False) -> Starlette:
-    """Create a Starlette application that can serve the provied mcp server with SSE."""
-    sse = SseServerTransport("/messages/")
-
-    async def handle_sse(request: Request) -> None:
-        async with sse.connect_sse(
-                request.scope,
-                request.receive,
-                request._send,  # noqa: SLF001
-        ) as (read_stream, write_stream):
-            await mcp_server.run(
-                read_stream,
-                write_stream,
-                mcp_server.create_initialization_options(),
-            )
-
-    return Starlette(
-        debug=debug,
-        routes=[
-            Route("/sse", endpoint=handle_sse),
-            Mount("/messages/", app=sse.handle_post_message),
-        ],
-    )
-
-
 STDIO_SERVER = False
-# execute and return the stdio output
 if __name__ == '__main__':
-    # if STDIO_SERVER:
-    #     mcp.run(transport='stdio')
-    # else:  # http server
-    #     mcp_server = mcp._mcp_server
-    #     starlette_app = create_starlette_app(mcp_server, debug=True)
-    #     uvicorn.run(starlette_app, host='0.0.0.0', port=8080)
-
-    app = Starlette(
-        debug=False,
-        routes=[
-            Mount('/', app=mcp.sse_app()),
-        ],
-    )
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    if STDIO_SERVER:
+        mcp.run(transport='stdio')
+    else:  # http server
+        app = Starlette(
+            debug=False,
+            routes=[
+                Mount('/', app=mcp.sse_app()),
+            ],
+        )
+        uvicorn.run(app, host='0.0.0.0', port=8080)
